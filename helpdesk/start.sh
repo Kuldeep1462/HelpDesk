965098b6ace4804c55e9362a32501508bbba30e7
Kuldeep Singh <kuldeepsinghparhar01@gmail.com>
2025-10-06 12:20:38 +0530

    updated start.sh

--- helpdesk/start.sh
+++ helpdesk/start.sh
@@ -7,5 +7,13 @@ from django.contrib.auth import get_user_model;
 User = get_user_model();
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+if not User.objects.filter(username='Admin12').exists():
+    user = User.objects.create_user(username='Admin12', email='admin@gmail.com', password='admin@123')
+    user.role = 'admin'
+    user.save()
+if not User.objects.filter(username='agent12').exists():
+    user = User.objects.create_user(username='agent12', email='agent@gmail.com', password='agent@123')
+    user.role = 'agent'
+    user.save()
 "
 python manage.py runserver 0.0.0.0:$PORT
